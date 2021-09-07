e7f88df377e46cb42d77fa18379cd6cd5ff218cb
Shihab Jamil <sjamil181180@bscse.uiu.ac.bd>
2021-09-07 15:12:59 +0600

    cron_job updated

--- cron_job.php
+++ cron_job.php
@@ -7,7 +7,7 @@
 
    foreach ($list_delivery_boys as $row) {
        $delivery_boy_id = $row['id'];
-       $target = $row['target'];
+       $target = $row['monthly_target'];
        $pct = $row['pct_per_parcel'];
        $query = "INSERT INTO `salary`( `delivery_boy_id`, `date`, `target`, `pct_amount`, `total_salary`) VALUES ($delivery_boy_id, $current_date, $target, $pct, 0)";
        mysqli_query($con, $query); 
